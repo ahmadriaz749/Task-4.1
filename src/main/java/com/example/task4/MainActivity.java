f726ff76e1e6d90596b52ad7bbfdb5af9365d947
ahmadriaz749 <82609261+ahmadriaz749@users.noreply.github.com>
2023-04-28 17:36:16 +1000

    Add files via upload

---
+++ src/main/java/com/example/task4/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.task4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cv_tmp;
+    CardView cv_weight;
+    CardView cv_length;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cv_tmp = findViewById(R.id.cv_tmp);
+        cv_weight = findViewById(R.id.cv_weight);
+        cv_length = findViewById(R.id.cv_length);
+
+        cv_tmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Start_Workout.class));
+            }
+        });
+        cv_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Start_Workout.class));
+            }
+        });
+        cv_length.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Start_Workout.class));
+            }
+        });
+
+
+    }
+}
